2ee9eef14e49868c56739bef03ae5b1f61c7503f
WMJ-super1 <774167972@qq.com>
2019-09-18 13:28:33 +0800

    a document on Caculate

---
+++ Caculate.java
@@ -0,0 +1,14 @@
+public class Caculate{
+    public static void main(String[] args){
+		System.out.println("34 + 30 =" +"34"+"30");
+        int n1 = 3;
+		int n2 = 5;
+		double res = (double)n1/n2;
+        System.out.println(res);
+		int count = 10;
+	    count *= count + 2;
+		count /= 100;
+		count++;
+		System.out.println(count);
+	}
+}
